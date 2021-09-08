a8ff72eafe592cc9ec5cf5db589a573da5de7ae9
md jahidul islam <za0162hid@gmail.com>
2021-09-08 20:40:52 +0600

    partnered added

--- components/Partnered/Partnered.js
+++ components/Partnered/Partnered.js
@@ -2,76 +2,42 @@ import React from "react";
 
 const Partnered = () => {
   return (
-    <div className="w-full bg-blue-400 pt-5 pb-14">
+    <div className="w-full bg-blue-400 pt-24 pb-14">
       <h1 className="text-center font-bold text-4xl pt-5 text-white mb-14">
         Some of Publisher we partnered for
       </h1>
-      <div>
+      <div className="px-2">
         <div className="flex justify-around  flex-wrap">
           <img
-            className="w-36 h-10"
-            src="https://ehya.designspace.io/assets/images/clients-logo/slack-white.svg"
-            alt=""
-          />
-
-          <img
-            className="w-36 h-10"
-            src="https://ehya.designspace.io/assets/images/clients-logo/netflix-white.svg"
-            alt=""
-          />
-
-          <img
-            className="w-36 h-10"
-            src="https://ehya.designspace.io/assets/images/clients-logo/fitbit-white.svg"
-            alt=""
-          />
-
-          <img
-            className="w-36 h-10"
-            src="https://ehya.designspace.io/assets/images/clients-logo/google-white.svg"
-            alt=""
-          />
-
-          <img
-            className="w-36 h-10"
-            src="https://ehya.designspace.io/assets/images/clients-logo/airbnb-white.svg"
+            className="w-36 h-20"
+            src="/assets/partnersImg/download.jpeg"
             alt=""
           />
           <img
-            className="w-36 h-10"
-            src="https://ehya.designspace.io/assets/images/clients-logo/uber-white.svg"
+            className="w-36 h-20"
+            src="/assets/partnersImg/download1.jpeg"
+            alt=""
+          />
+          <img
+            className="w-36 h-20"
+            src="/assets/partnersImg/download2.jpeg"
             alt=""
           />
         </div>
         <div className="flex justify-around  flex-wrap mt-16">
           <img
-            className="w-36 h-10"
-            src="https://ehya.designspace.io/assets/images/clients-logo/uber-white.svg"
+            className="w-36 h-20"
+            src="/assets/partnersImg/download3.jpeg"
             alt=""
           />
           <img
-            className="w-36 h-10"
-            src="https://ehya.designspace.io/assets/images/clients-logo/airbnb-white.svg"
+            className="w-36 h-20"
+            src="/assets/partnersImg/download4.jpeg"
             alt=""
           />
           <img
-            className="w-36 h-10"
-            src="https://ehya.designspace.io/assets/images/clients-logo/google-white.svg"
-            alt=""
-          />
-          <img
-            className="w-36 h-10"
-            src="https://ehya.designspace.io/assets/images/clients-logo/fitbit-white.svg"
-            alt=""
-          />
-          <img
-            className="w-36 h-10"
-            src="https://ehya.designspace.io/assets/images/clients-logo/netflix-white.svg"
-            alt=""
-          />
-          <img
-            className="w-36 h-10"
-            src="https://ehya.designspace.io/assets/images/clients-logo/slack-white.svg"
+            className="w-36 h-20"
+            src="/assets/partnersImg/download5.jpeg"
             alt=""
           />
         </div>
