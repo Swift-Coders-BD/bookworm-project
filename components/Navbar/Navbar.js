e9829929a4e3e1259004b394f1c10e6c34d8dc6c
md jahidul islam <za0162hid@gmail.com>
2021-09-09 10:55:27 +0600

    navbar error fixed

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -40,15 +40,15 @@ const Header = () => {
           <svg
             className="w-10"
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
+            className="h-6 w-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M4 6h16M4 12h16M4 18h16"
             />
           </svg>
@@ -74,7 +74,7 @@ const Header = () => {
                 {/* <span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="h-6 w-5"
+                    className="h-6 w-5"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
@@ -89,37 +89,37 @@ const Header = () => {
             </a>
 
             {/* this is dropdown */}
-            {/* <div class="group inline-block relative">
-              <div class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-600 font-bold items-center justify-center hover:hover:bg-red-500 hover:text-white">
-                <span class="mr-1">Categories</span>
+            {/* <div className="group inline-block relative">
+              <div className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-600 font-bold items-center justify-center hover:hover:bg-red-500 hover:text-white">
+                <span className="mr-1">Categories</span>
                 <svg
-                  class="fill-current h-4 w-4"
+                  className="fill-current h-4 w-4"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                 >
                   <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />{" "}
                 </svg>
               </div>
-              <ul class="absolute hidden group-hover:block text-gray-700 lg:pt-5">
-                <li class="">
+              <ul className="absolute hidden group-hover:block text-gray-700 lg:pt-5">
+                <li className="">
                   <a
-                    class="rounded-t bg-gray-200 text-gray-600 hover:hover:bg-red-500 hover:text-white py-3 px-24 block whitespace-no-wrap"
+                    className="rounded-t bg-gray-200 text-gray-600 hover:hover:bg-red-500 hover:text-white py-3 px-24 block whitespace-no-wrap"
                     href="#"
                   >
                     Story books
                   </a>
                 </li>
-                <li class="">
+                <li className="">
                   <a
-                    class="bg-gray-200 text-gray-600 hover:hover:bg-red-500 hover:text-white py-3 px-24 block whitespace-no-wrap"
+                    className="bg-gray-200 text-gray-600 hover:hover:bg-red-500 hover:text-white py-3 px-24 block whitespace-no-wrap"
                     href="#"
                   >
                     Nobel books
                   </a>
                 </li>
-                <li class="">
+                <li className="">
                   <a
-                    class="rounded-b bg-gray-200 text-gray-600 hover:hover:bg-red-500 hover:text-white py-3 px-24 block whitespace-no-wrap"
+                    className="rounded-b bg-gray-200 text-gray-600 hover:hover:bg-red-500 hover:text-white py-3 px-24 block whitespace-no-wrap"
                     href="#"
                   >
                     Text books
