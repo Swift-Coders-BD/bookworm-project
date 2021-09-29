5022b6a7912304e1f8a87b9a657afd29d0897b1b
unknown <za0162hid@gmail.com>
2021-09-29 19:17:42 +0600

    subscribe added

---
+++ .history/components/Subscribe/Subscribe_20210928213511.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Subscribe = () => {
+    return (
+        <div>
+            <div>
+              hello world 
+     we are the 
+                              <tr>element </tr>
+                and the 
+            </div>
+        </div>
+    );
+};
+
+export default Subscribe;
---
+++ .history/components/Subscribe/Subscribe_20210928213842.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Subscribe = () => {
+    return (
+        <div>
+           hello <tr>heloo this is the best team</tr> 
+        </div>
+    );
+};
+
+export default Subscribe;
